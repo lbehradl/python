4b5c595a12ac9cf386780f0da9981e3f4d4f2aae
lbehradl <157172270+lbehradl@users.noreply.github.com>
2024-10-09 22:47:40 +0330

    Add files via upload

---
+++ 1. OOP/3. Object-oriented project.py
@@ -0,0 +1,25 @@
+import random
+class Human:
+    def __init__(self, name):
+        self.name = name
+
+class Player(Human):
+    def __init__(self, name, team):
+        Human.__init__(self, name)
+        self.team = team
+        
+player_names = ["حسین", "مازیار", "اکبر", "نیما", "مهدی"
+                , "فرهاد", "محمد", "خشایار", "میلاد", "مصطفی", "امین"
+                , "سعید", "پویا", "پوریا", "رضا", "علی", "بهزاد"
+                , "سهیل", "بهروز", "شهروز", "سامان", "محسن"]
+
+random.shuffle(player_names)
+player = []
+
+for i in range(22):
+    team_name = "Team Aval" if i<11 else "Team Dovom"
+    player.append(Player(player_names[i],team_name))
+    
+for person in player:
+    print(person.name,person.team)
+
